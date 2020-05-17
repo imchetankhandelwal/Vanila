32c0293ffbbbc2b38897f542b84f2e6bd4b834b7
imchetankhandelwal <64724886+imchetankhandelwal@users.noreply.github.com>
2020-05-18 00:45:16 +0530

    Test
    Test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
+  info about Parcel  fdfdfd
+  
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
